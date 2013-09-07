omnirom/android_device_sony_mint

Initial support for the hardware tunables framework
Change-Id: I5c1bc1950177a214e87017cd73868847dd68fb26

BoardConfig.mk
@@ -25,5 +25,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 
 TARGET_OTA_ASSERT_DEVICE := LT30p,mint
 
+BOARD_HARDWARE_CLASS := device/sony/mint/cmhw
+
 # inherit from the proprietary version
 -include vendor/sony/mint/BoardConfigVendor.mk

ObiJuanKenobe/device_sony_tsubasa

BoardConfig.mk
@@ -25,5 +25,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 
 TARGET_OTA_ASSERT_DEVICE := LT25i,tsubasa
 
+BOARD_HARDWARE_CLASS := device/sony/tsubasa/cmhw
+
 # inherit from the proprietary version
 -include vendor/sony/tsubasa/BoardConfigVendor.mk